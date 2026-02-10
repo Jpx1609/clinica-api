ef7f793063696a09ee1bed92727776990313fe92
Jpx1609 <joaopedro160910@gmail.com>
2026-02-10 19:44:31 -0300

    Add rota UPDATE e DELETE (CRUD Completo)

--- src/main/java/com/example/clinicaapi/controllers/PacienteController.java
+++ src/main/java/com/example/clinicaapi/controllers/PacienteController.java
@@ -13,14 +13,30 @@ public class PacienteController {
 
     @Autowired
     private PacienteRepository repository;
-
+    // LISTAR TODOS OS PACIENTES
     @GetMapping
     public List<Paciente> listarTodos() {
         return repository.findAll();
     }
-
+    // ADICIONAR UM PACIENTE
     @PostMapping
     public Paciente salvar(@RequestBody Paciente paciente) {
         return repository.save(paciente);
     }
-}
+    // DELETAR UM PACIENTE
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+    // ATUALIZAR UM PACIENTE
+    @PutMapping("/{id}")
+    public Paciente atualizar(@PathVariable Long id, @RequestBody Paciente pacienteAtualizado) {
+        return repository.findById(id).map(paciente  -> {
+            paciente.setNome(pacienteAtualizado.getNome());
+            paciente.setCpf(pacienteAtualizado.getCpf());
+            paciente.setEmail(pacienteAtualizado.getEmail());
+
+            return repository.save(paciente);
+        }).orElse(null);
+        }
+    }
